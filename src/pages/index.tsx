2daf122a0f05e60e5ffbd5e8d07457a83d8eb9de
Mednoob <ahmadyaqdhan123@gmail.com>
2024-07-01 17:05:12 +0900

    feat: modify main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
-    return <div className="w-screen h-screen flex items-center justify-center">
-        <p className="font-bold text-3xl">Hello World!</p>
+    return <div className="w-screen-no-scroll h-screen flex items-center justify-center bg-gray-300">
+        <p className="font-bold text-3xl animate-bounce-up">Hello World!</p>
     </div>
 }
