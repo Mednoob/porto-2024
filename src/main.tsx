6a6b1ce7ad2e0109821f2161f7beefbbf9211ba8
Mednoob <ahmadyaqdhan123@gmail.com>
2024-07-01 16:21:52 +0900

    feat: tidy up

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import React from "react";
+
+import Home from './pages/index';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
 )
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+    return <div className="w-screen h-screen flex items-center justify-center">
+        <p className="font-bold text-3xl">Hello World!</p>
+    </div>
+}
