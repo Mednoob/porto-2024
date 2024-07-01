20cbd3e875a75effffe8cf6413bf8eb948d11a28
Mednoob <ahmadyaqdhan123@gmail.com>
2024-07-01 17:05:22 +0900

    feat: add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return <div className="w-screen-no-scroll h-screen flex items-center justify-center bg-gray-300">
+        <div className="text-center font-bold text-3xl animate-bounce-up">
+            <p>404 Not Found</p>
+            <p>¯\_(ツ)_/¯</p>
+        </div>
+    </div>
+}
